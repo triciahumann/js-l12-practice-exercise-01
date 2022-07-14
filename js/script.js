8317638c9dee86585371aad435d10d68c859241b
Tricia H <101357697+triciahumann@users.noreply.github.com>
2022-07-14 09:20:18 -0700

    Updated JS to pull images, names, and locations for users

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 const randomFolks = document.querySelector(".random-peeps");
+
+const getData = async function () {
+    const usersRequest = await fetch (
+        "https://randomuser.me/api?results=5"
+    );
+    const data = await usersRequest.json();
+    const userResults = data.results; // array of objects
+    displayUsers(userResults);
+    //console.log(userResults); log out to make sure i got the array
+};
+
+getData();
+
+const displayUsers = function (userResults) {
+    randomFolks.innerHTML = "";
+    for (let user of userResults) {
+        const country = user.location.country;
+        const name = user.name.first;
+        const imageURL = user.picture.medium;
+        const userDiv = document.createElement("div")
+            userDiv.innerHTML = `
+                <h3>$${name}</h3>
+                <p>${country}</p>
+                <img src=${imageURL} alt = "User avatar" />
+            `;
+        randomFolks.append(userDiv);
+    }
+};
